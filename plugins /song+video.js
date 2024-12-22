f8a6daae27fbd53bd47b9a367ef38ebe5e31f0c3
Sanju Bro <96864439+Sanjuwa22@users.noreply.github.com>
2024-12-22 14:25:55 +0530

    Update song+video.js

--- plugins /song+video.js
+++ plugins /song+video.js
@@ -1,1 +1,45 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply ("please give me url or title")
+  const search = await yts(q)
+  const data = search.videos[0];
+const url = data.url
 
+  let desc = ' 
+    🤖 Sanju Bro 🇱🇰  ᴠͥɪͣ⃢ᴘͫ  ✮ Song Downloader 📥
+
+▶ Title : ${data.title}
+  ▶ Description : ${data.description}
+  ▶ Time : ${data.timestamp}
+▶ Ago : ${data.ago}
+  ▶ Views : ${data.views}
+
+  > @▶ Sanju Bro 🇱🇰  ᴠͥɪͣ⃢ᴘͫ  ✮
+'
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+  //download audio 
+  let down = await fg.yta(url) 
+  let downloadurl = down.dl_url
+  // send audio message
+  
+await conn.sendMessage(from,{audio:{url:{downloadurl},mimetype:"audio/mpeg"},{quoted:mek});
+  
+
+
+
+}catch(e){
+  console.log(e)
+  reply("s{e}")
+
+}
+})
