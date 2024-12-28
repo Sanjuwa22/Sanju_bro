d8f919237794c314324650a89c6b3a08929db924
Sanju Bro <96864439+Sanjuwa22@users.noreply.github.com>
2024-12-28 10:56:37 +0530

    Update conflg.js

--- conflg.js
+++ conflg.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || ""
 ALIVE_IMG: process. env.ALIVE_IMG || "https://i.ibb.co/QMBc3qP/2771.jpg",
 ALIVE_MSG: process. env.ALIVE_MSG || "> @ *🦅✨ᴅᴇꜱɪɢɴ ʙʏ ꜱᴀɴᴊᴜ ʙʀᴏ ᴛᴇᴀᴍ ᴏꜰ ᴛʜᴇ ᴅᴀʀᴋ ᴄʏʙᴇʀ ᴍᴀᴋᴇʀ & ʙʟᴀᴄᴋ ᴍᴀʀᴋᴇᴛ. ✨🇱🇰*
 > @📱 *wa.me/94710513877*",   
